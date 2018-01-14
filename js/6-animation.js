fc8e205b1dc758ca9250c38708c985fd81b24832
Malgorzata Maciejowska <malgorzatamaciejowska21@gmail.com>
2018-01-14 14:48:44 +0100

    animacje

---
+++ js/6-animation.js
@@ -0,0 +1,40 @@
+'use strict';
+$(function () {
+    //    $('p').click(function(){
+    ////       $(this).fadeOut(1000).fadeIn(1000);
+    //       $('p').fadeOut(1000, /*function(){alert('Hide skonczony')}*/).fadeIn(1000); // dla wszystkich p
+    //    
+    //    });
+    //    $('h1').hide(1000).show(1000);
+    //    
+    //    $('h2').slideUp(3000).slideDown(2000);
+//    });
+
+    function animacja() {
+        console.log('No i dotarlismy do konca, tu wywoluje sie funkcja callback');
+        $('h3').animate({
+            'font-size': '5px',
+            'margin-left': '0'
+        }, 3000);
+    }
+
+    $('h3').animate({
+        'font-size': '40px',
+        'margin-left': '100px'
+    }, 4000, animacja);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
